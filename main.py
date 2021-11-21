f6da163cda657502da47646314240266159442c6
Sahaj Singh <49692422+SatireSage@users.noreply.github.com>
2021-11-20 23:50:41 -0800

    shtuff

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import re
-import sys
 import pyttsx3
 from latext import latex_to_text as lat
 
@@ -20,9 +18,9 @@ def fraction(substr):
     while num_braces < 2:
         if substr[index] == "}":
             num_braces += 1
-        if num_braces == 0 and substr[index] != "{":
+        if num_braces == 0 and substr[index] != "{" and substr[index] != "}":
             numerator += substr[index]
-        elif num_braces == 1 and substr != "{":
+        elif num_braces == 1 and substr[index] != "{" and substr[index] != "}":
             denominator += substr[index]
         old += substr[index]
         index += 1
@@ -31,17 +29,19 @@ def fraction(substr):
 
 
 def integral(substr):
-    # \int_{-1}^x (3 + t^2)\,dt #TODO:: possibly replace dt
     # if there is an underscore there are limits
     limitIndex = substr.find('_')
     if limitIndex == -1:
         return substr.replace('\int', 'integral of ')
 
     limitIndex += 1
+
     lim_a = ""
     lim_b = ""
     term_a_done = False
     old = "\int_"
+    old += substr[limitIndex]
+
     while substr[limitIndex] != " ":
         if substr[limitIndex] == "^":
             term_a_done = True
@@ -52,18 +52,20 @@ def integral(substr):
         limitIndex += 1
         old += substr[limitIndex]
 
-    return substr.replace(old, 'integral from' + lim_a + ' to ' + lim_b + " ")
+    return substr.replace(old, 'integral from' + lim_a + ' to ' + lim_b + " of ")
 
 
 def dollarSign(mathStr):
-    mathStr = mathStr.replace("\displaystyle","")
-    # {A(x) = \int_{-1}^x (3 + t^2)\,dt}
-    if ('int_' in mathStr):
+    mathStr = mathStr.replace('\displaystyle', "")
+    if ('int' in mathStr):
         result = integral(mathStr)
-        return result
+        if ('frac' in mathStr):
+            result = fraction(result)
+    elif ('frac' in mathStr):
+        result = fraction(mathStr)
     else:
-        return mathStr
-    return ""
+        result = mathStr
+    return result
 
 
 def mathParse(strLine):
@@ -73,9 +75,10 @@ def mathParse(strLine):
 
         # split string by $ to interpret math
         if '$' in strLine:
-            splitStrings = strLine.split('$')
+            dlim = '$'
+            splitStrings = [e + dlim for e in strLine.split(dlim) if e]
             for i in range(int(len(splitStrings) / 2)):
-                splitStrings[2*i + 1] = dollarSign(splitStrings[2*i + 1])
+                splitStrings[2 * i + 1] = dollarSign(splitStrings[2 * i + 1])
 
             # rejoin string sections
             strLine = " ".join(splitStrings)
@@ -89,10 +92,8 @@ def mathParse(strLine):
 
 if __name__ == '__main__':
     engine = pyttsx3.init()
-
     f = open("testFile2.tex", 'r')
     lines = f.readlines()
-
     println = False
     num = 0
     for line in lines:
@@ -102,8 +103,7 @@ if __name__ == '__main__':
             println = False
         if println:
             strippedLine = mathParse(line)
-
-            print(str(num) + " " + strippedLine)
+            print(strippedLine)
             engine.say(strippedLine)
             engine.runAndWait()
             num += 1
